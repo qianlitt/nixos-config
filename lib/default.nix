3676f936044e7503e470a05ed8cd7d98a764cd82
qianlitt <zvdiek@gmail.com>
2026-03-07 21:24:10 +0800

    feat: 添加工具函数 `scanModules` 等
    可以批量导入和排除模块。

--- lib/default.nix
+++ lib/default.nix
@@ -1,4 +1,8 @@
-{lib, ...}: {
-  # 以项目录根目录为基础路径
-  root = lib.path.append ../.;
-}
+{lib, ...}: let
+  scanUtils = import ./scan-modules.nix {inherit lib;};
+in
+  scanUtils
+  // {
+    # 以项目录根目录为基础路径
+    root = lib.path.append ../.;
+  }
