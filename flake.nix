06fed8045898aca1b0bea2c00373d519182b12df
qianlitt <zvdiek@gmail.com>
2026-03-07 21:46:49 +0800

    feat: 添加 disko 配置
    使用 disko 管理磁盘，并设置 zram。

--- flake.nix
+++ flake.nix
@@ -4,6 +4,11 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
+
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs @ {
