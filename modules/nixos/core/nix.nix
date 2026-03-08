881ef5709c2d16ee15b30c0950a82161eab0e80b
qianlitt <zvdiek@gmail.com>
2026-03-08 16:29:36 +0800

    refactor: 将 nix 相关配置提取为单独配置
    - 允许安装 unfree 软件。
    - 启用 `nix-command` 和 `flakes`。
    - 启用每周自动垃圾回收 (GC)，清理 7 天前的旧版本。
    - 启用每日定时存储优化 (nix optimise) 以节省磁盘空间。

---
+++ modules/nixos/core/nix.nix
@@ -0,0 +1,29 @@
+{myvar, ...}: {
+  nixpkgs.config.allowUnfree = true;
+  nix = {
+    settings = {
+      experimental-features = ["nix-command" "flakes"];
+
+      trusted-users = [myvar.user.name];
+      substituters = [
+        # cache mirror located in China
+        # status: https://mirror.sjtu.edu.cn/
+        "https://mirror.sjtu.edu.cn/nix-channels/store/"
+        # status: https://mirrors.ustc.edu.cn/status/
+        "https://mirrors.ustc.edu.cn/nix-channels/store/"
+      ];
+    };
+
+    # 每周自动垃圾回收
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+    # 定时优化存储
+    optimise = {
+      automatic = true;
+      dates = ["03:45"];
+    };
+  };
+}
