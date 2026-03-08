dd1c2fd23cf7e8e938459488452dfb5cd8874616
qianlitt <zvdiek@gmail.com>
2026-03-08 16:29:36 +0800

    refactor: 将 nix 相关配置提取为单独配置
    - 允许安装 unfree 软件。
    - 启用 `nix-command` 和 `flakes`。
    - 启用每周自动垃圾回收 (GC)，清理 7 天前的旧版本。
    - 启用每日定时存储优化 (nix optimise) 以节省磁盘空间。

--- hosts/server-frieren/default.nix
+++ hosts/server-frieren/default.nix
@@ -42,10 +42,6 @@ in {
     variant = "";
   };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-  nix.settings.experimental-features = ["nix-command" "flakes"];
-
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
