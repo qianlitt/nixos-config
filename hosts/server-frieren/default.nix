6719ec9b32686046a60b76f9eef5a1aa9a216a85
qianlitt <zvdiek@gmail.com>
2026-03-08 20:21:01 +0800

    refactor: 模块化基础 program 配置

--- hosts/server-frieren/default.nix
+++ hosts/server-frieren/default.nix
@@ -48,35 +48,6 @@ in {
     variant = "";
   };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    neovim
-    wget
-    git
-
-    # nix
-    nil
-    alejandra
-  ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
-  };
-  programs.nix-ld.enable = true;
-  programs.direnv = {
-    enable = true;
-    nix-direnv.enable = true;
-
-    enableBashIntegration = true;
-    enableFishIntegration = true;
-    enableZshIntegration = true;
-  };
-
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
