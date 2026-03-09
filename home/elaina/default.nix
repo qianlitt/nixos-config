16c2a5ca7d1cf94a315ab06e62e4f942babbf7bf
qianlitt <zvdiek@gmail.com>
2026-03-09 17:34:40 +0800

    feat: 添加可复用的 Git 配置模块
    - 新增 `modules/home-manager/core/git` 模块
    - 支持通过选项配置 Git 用户、签名密钥和 TUI 工具
    - 引入 delta 作为语法高亮 pager
    - 在 elaina 用户配置中启用新模块

--- home/elaina/default.nix
+++ home/elaina/default.nix
@@ -1,6 +1,25 @@
-{mylib, ...}: {
+{
+  mylib,
+  myvar,
+  ...
+}: {
   imports =
-    mylib.scanModules ./.;
+    mylib.scanModules ./.
+    ++ [(mylib.root "modules/home-manager/core")];
+
+  # git
+  modules.git = {
+    enable = true;
+    user = {
+      name = myvar.git.user.name;
+      email = myvar.git.user.email;
+    };
+    signing = {
+      enable = true;
+      key = myvar.git.signingKey;
+    };
+    tui.enable = true;
+  };
 
   home.stateVersion = "25.11";
 }
--- var/default.nix
+++ var/default.nix
@@ -1,4 +1,4 @@
-{
+rec {
   user = {
     name = "elaina";
     email = "zvdiek@gmail.com";
@@ -6,5 +6,13 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIql1DkkFW4n1tntQxVblT3+9Lv/d8hm7i7JWZEfzrbx bitwarden@nixos"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICqTv14u4PA/Vox3HYIthOutdu9u6wSOVTWPAUf+2Gd5 openpgp"
     ];
+  };
+
+  git = {
+    user = {
+      name = "qianlitt";
+      email = user.email;
+    };
+    signingKey = "C6D152AC471FA822";
   };
 }
