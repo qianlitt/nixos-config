b448fd4ed2208ce4e5aa9b8e9098b87d744df7d7
qianlitt <zvdiek@gmail.com>
2026-03-10 18:41:59 +0800

    feat: 将 fish 设置为用户的默认 shell

--- modules/nixos/core/user.nix
+++ modules/nixos/core/user.nix
@@ -13,11 +13,14 @@
     "user/${myvar.user.name}/hashedPassword" = {neededForUsers = true;};
   };
 
+  programs.fish.enable = true;
+
   users.users."${myvar.user.name}" = {
     isNormalUser = true;
     description = "${myvar.user.name} - main user";
     extraGroups = ["networkmanager" "wheel"];
     packages = with pkgs; [];
+    shell = pkgs.fish;
     openssh.authorizedKeys.keys = myvar.user.authorizedKeys;
     hashedPasswordFile = config.sops.secrets."user/${myvar.user.name}/hashedPassword".path;
   };
