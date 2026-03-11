a0437492d4531f393555d4ca985da5ac0249f5c6
qianlitt <zvdiek@gmail.com>
2026-03-11 18:36:19 +0800

    refactor: 使用 systemd-networkd 配置网络
    - 新增 var/networking.nix 集中管理网络配置变量。
    - 新增 modules/nixos/core/networking.nix 实现 systemd-networkd 配置模块
    - 修改 hosts/server-frieren/default.nix 采用新的网络配置

---
+++ var/networking.nix
@@ -0,0 +1,12 @@
+rec {
+  gateway = "192.168.1.200";
+  dns = ["192.168.1.200"];
+  prefixLength = 24;
+
+  host = {
+    server-frieren = {
+      hostName = "frieren";
+      ip = "192.168.1.102";
+    };
+  };
+}
