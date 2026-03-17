35a1b70de993c232a80d768f471cff49dd98bbd8
qianlitt <zvdiek@gmail.com>
2026-03-17 13:01:25 +0800

    feat: 添加 JProxy 模块
    JProxy 用于增强 arr 家族的搜索能力。

---
+++ modules/nixos/services/jproxy.nix
@@ -0,0 +1,50 @@
+{
+  config,
+  lib,
+  ...
+}: let
+  cfg = config.modules.nixos.jproxy;
+in {
+  options.modules.nixos.jproxy = {
+    enable = lib.mkEnableOption ''
+      启用 JProxy 服务
+
+      介于 Sonarr / Radarr 和 Jackett / Prowlarr 之间的代理，主要用于优化查询和提升识别率。
+    '';
+
+    port = lib.mkOption {
+      type = lib.types.port;
+      default = 8117;
+      description = "JProxy 服务端口";
+    };
+
+    dataDir = lib.mkOption {
+      type = lib.types.path;
+      default = "/var/lib/jproxy";
+      description = "JProxy 服务数据目录";
+    };
+  };
+  config = lib.mkIf cfg.enable {
+    # 创建数据目录
+    systemd.tmpfiles.rules = [
+      "d '${toString cfg.dataDir}' 0755 root root - -"
+    ];
+
+    # 配置 quadlet 容器
+    virtualisation.quadlet.containers.jproxy = {
+      autoStart = true;
+      containerConfig = {
+        image = "docker.io/luckypuppy514/jproxy";
+        publishPorts = ["${toString cfg.port}:8117"];
+        volumes = ["${toString cfg.dataDir}:/config"];
+        environments = {
+          "TZ" = config.time.timeZone;
+          "JAVA_OPTS" = "-Xms512m -Xmx512m";
+        };
+      };
+      serviceConfig = {
+        Restart = "always";
+      };
+    };
+  };
+}
