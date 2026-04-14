cc032076b03fa98f8dcd5b6d8d9809b94669e1db
qianlitt <zvdiek@gmail.com>
2026-04-14 21:56:00 +0800

    refactor: 模块化 hyprland

--- modules/nixos/core/nix.nix
+++ modules/nixos/core/nix.nix
@@ -9,13 +9,7 @@
         # cache mirror located in China
         # status: https://mirrors.ustc.edu.cn/status/
         "https://mirrors.ustc.edu.cn/nix-channels/store/"
-
-        # Hyprland cachix
-        "https://hyprland.cachix.org"
       ];
-
-      trusted-substituters = ["https://hyprland.cachix.org"];
-      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
     };
 
     # 每周自动垃圾回收
