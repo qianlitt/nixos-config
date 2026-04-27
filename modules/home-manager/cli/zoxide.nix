d956ac499c6b5148116d29c66041e3907d6da26f
qianlitt <zvdiek@gmail.com>
2026-04-27 10:37:16 +0800

    feat: 添加 zoxide 配置

---
+++ modules/home-manager/cli/zoxide.nix
@@ -0,0 +1,13 @@
+/**
+* zoxide - 智能目录跳转工具
+*/
+{
+  programs.zoxide = {
+    enable = true;
+
+    enableBashIntegration = true;
+    enableFishIntegration = true;
+    enableNushellIntegration = true;
+    enableZshIntegration = true;
+  };
+}
