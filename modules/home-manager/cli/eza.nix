f41e3e3a3af014c7961a4f9915d54f34d0f602fd
qianlitt <zvdiek@gmail.com>
2026-04-27 16:22:25 +0800

    feat: 添加 eza 配置

---
+++ modules/home-manager/cli/eza.nix
@@ -0,0 +1,10 @@
+{
+  programs.eza = {
+    enable = true;
+
+    enableBashIntegration = true;
+    enableFishIntegration = true;
+    enableNushellIntegration = true;
+    enableZshIntegration = true;
+  };
+}
