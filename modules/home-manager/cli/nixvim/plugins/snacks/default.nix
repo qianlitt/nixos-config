385daea9b1be88c5f04b98ecfc560dcdb6c5c1f6
qianlitt <zvdiek@gmail.com>
2026-05-02 11:56:54 +0800

    feat(nixvim): 添加 snacks.nvim 剩余组件配置

--- modules/home-manager/cli/nixvim/plugins/snacks/default.nix
+++ modules/home-manager/cli/nixvim/plugins/snacks/default.nix
@@ -6,5 +6,43 @@
 {mylib, ...}: {
   imports = mylib.scanModules ./.;
 
-  programs.nixvim.plugins.snacks.enable = true;
+  programs.nixvim = {
+    plugins.snacks = {
+      enable = true;
+
+      settings = {
+        bigfile.enabled = true; # 对大文件优化
+        indent.enabled = false; # 缩进线
+        input.enabled = true; # 输入框
+        notifier.enabled = true; # 通知
+        quickfile.enabled = true; # 尽快加载文件
+        scope.enabled = true; # 检测当前行所属的 scope
+        scroll.enabled = true; # 平滑滚动
+        statuscolumn.enabled = false; # 状态栏
+        words.enabled = true; # LSP 引用查看和跳转
+      };
+    };
+
+    keymaps = [
+      # stratch - 临时缓冲区
+      # Docs: https://github.com/folke/snacks.nvim/blob/main/docs/scratch.md
+      {
+        key = "<leader>.";
+        action.__raw = "function() Snacks.scratch() end";
+        options.desc = "Toggle Scratch Buffer";
+      }
+      {
+        key = "<leader>S";
+        action.__raw = "function() Snacks.scratch.select() end";
+        options.desc = "Select Scratch Buffer";
+      }
+
+      # notifier
+      {
+        key = "<leader>un";
+        action.__raw = "function() Snacks.notifier.hide() end";
+        options.desc = "Dismiss All Notifications";
+      }
+    ];
+  };
 }
