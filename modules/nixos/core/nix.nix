e7b94b4e775d6f3d5c60fc8f8c3b2b16235d2777
qianlitt <zvdiek@gmail.com>
2026-05-10 12:51:29 +0800

    refactor: 将 Nix access-tokens 配置移入 nixosModules 模块

--- modules/nixos/core/nix.nix
+++ modules/nixos/core/nix.nix
@@ -1,4 +1,13 @@
-{myvar, ...}: {
+{
+  config,
+  inputs,
+  myvar,
+  ...
+}: {
+  imports = [
+    inputs.sops-nix.nixosModules.sops
+  ];
+
   nixpkgs.config.allowUnfree = true;
   nix = {
     settings = {
@@ -24,4 +33,25 @@
       dates = ["03:45"];
     };
   };
+
+  # 设置 Nix access-tokens
+  # Docs: https://nix.dev/manual/nix/latest/command-ref/conf-file#conf-access-tokens
+  sops = {
+    # 声明 token
+    # secrets.yaml 格式: github-token: ghp_xxx
+    secrets."github-token" = {
+      mode = "0400";
+    };
+
+    # 使用 sops-template 生成 access-tokens.conf 文件
+    templates."nix-access-tokens" = {
+      content = ''
+        access-tokens = github.com=${config.sops.placeholder."github-token"}
+      '';
+      path = "/etc/nix/access-tokens.conf";
+    };
+  };
+  nix.extraOptions = ''
+    !include /etc/nix/access-tokens.conf
+  '';
 }
