e9fa1c05eeab60617e2d960e073d2894bf54c484
qianlitt <zvdiek@gmail.com>
2026-05-11 20:57:31 +0800

    feat(nixvim): 划分 nixvim 键盘映射空间

--- modules/home-manager/cli/nixvim/plugins/which-key.nix
+++ modules/home-manager/cli/nixvim/plugins/which-key.nix
@@ -11,6 +11,85 @@
 
     settings = {
       preset = "helix"; # 外观: false | "classic" | "modern" | "helix"
+
+      # 映射空间
+      spec = [
+        {
+          __unkeyed-1 = "<leader><tab>";
+          group = "tabs";
+        }
+        {
+          __unkeyed-1 = "<leader>b";
+          group = "buffer";
+          expand.__raw = ''
+            function()
+              return require("which-key.extras").expand.buf()
+            end
+          '';
+        }
+        {
+          __unkeyed-1 = "<leader>c";
+          group = "code";
+        }
+        {
+          __unkeyed-1 = "<leader>d";
+          group = "debug";
+        }
+        {
+          __unkeyed-1 = "<leader>f";
+          group = "file/find";
+        }
+        {
+          __unkeyed-1 = "<leader>g";
+          group = "git";
+        }
+        {
+          __unkeyed-1 = "<leader>gh";
+          group = "hunks";
+        }
+        {
+          __unkeyed-1 = "<leader>q";
+          group = "quit/session";
+        }
+        {
+          __unkeyed-1 = "<leader>s";
+          group = "search";
+        }
+        {
+          __unkeyed-1 = "<leader>u";
+          group = "ui";
+        }
+        {
+          __unkeyed-1 = "<leader>w";
+          group = "window";
+          proxy = "<c-w>";
+          expand.__raw = ''
+            function()
+              return require("which-key.extras").expand.win()
+            end
+          '';
+        }
+        {
+          __unkeyed-1 = "<leader>x";
+          group = "diagnostics/quickfix";
+        }
+        {
+          __unkeyed-1 = "[";
+          group = "prev";
+        }
+        {
+          __unkeyed-1 = "]";
+          group = "next";
+        }
+        {
+          __unkeyed-1 = "g";
+          group = "goto";
+        }
+        {
+          __unkeyed-1 = "z";
+          group = "fold";
+        }
+      ];
     };
   };
 }
