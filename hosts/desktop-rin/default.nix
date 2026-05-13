91e692770f312a23b1bcbdab10fbaa606a479669
qianlitt <zvdiek@gmail.com>
2026-05-13 23:27:46 +0800

    feat: 为 rin 主机添加 btrfs scrub 服务
    定期验证 btrfs 文件系统的数据和元数据。

--- hosts/desktop-rin/default.nix
+++ hosts/desktop-rin/default.nix
@@ -63,6 +63,12 @@
     variant = "";
   };
 
+  # btrfs scrub
+  services.btrfs.autoScrub = {
+    enable = true;
+    interval = "weekly";
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
