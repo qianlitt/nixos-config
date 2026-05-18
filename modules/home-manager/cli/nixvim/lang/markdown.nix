73d393e15ec8a398b528d9c9961845b2725d6532
qianlitt <zvdiek@gmail.com>
2026-05-18 15:20:51 +0800

    feat(nixvim): 添加 markdown 的 lsp

--- modules/home-manager/cli/nixvim/lang/markdown.nix
+++ modules/home-manager/cli/nixvim/lang/markdown.nix
@@ -5,8 +5,12 @@
 }: let
   inherit (config.programs.nixvim.plugins.treesitter.package) builtGrammars;
 in {
-  programs.nixvim.plugins.treesitter.grammarPackages = lib.mkAfter [
-    builtGrammars.markdown
-    builtGrammars.markdown_inline
-  ];
+  programs.nixvim = {
+    lsp.servers.marksman.enable = true;
+
+    plugins.treesitter.grammarPackages = lib.mkAfter [
+      builtGrammars.markdown
+      builtGrammars.markdown_inline
+    ];
+  };
 }
