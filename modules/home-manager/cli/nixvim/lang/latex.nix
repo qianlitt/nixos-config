c3a432be1ef81e19d5201aa096b7a85c3605baed
qianlitt <zvdiek@gmail.com>
2026-05-18 12:17:51 +0800

    feat(nixvim): 添加 latex 的 lsp

--- modules/home-manager/cli/nixvim/lang/latex.nix
+++ modules/home-manager/cli/nixvim/lang/latex.nix
@@ -5,7 +5,11 @@
 }: let
   inherit (config.programs.nixvim.plugins.treesitter.package) builtGrammars;
 in {
-  programs.nixvim.plugins.treesitter.grammarPackages = lib.mkAfter [
-    builtGrammars.latex
-  ];
+  programs.nixvim = {
+    lsp.servers.texlab.enable = true;
+
+    plugins.treesitter.grammarPackages = lib.mkAfter [
+      builtGrammars.latex
+    ];
+  };
 }
