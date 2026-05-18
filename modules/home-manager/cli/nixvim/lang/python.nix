8d3f43e8a2b43ace20f6800eaf3d3076aa9cfc4e
qianlitt <zvdiek@gmail.com>
2026-05-18 23:04:51 +0800

    refactor(nixvim): 改用 `lsp.servers.<lsp>.config` 配置 LSP

--- modules/home-manager/cli/nixvim/lang/python.nix
+++ modules/home-manager/cli/nixvim/lang/python.nix
@@ -8,35 +8,19 @@
 in {
   programs.nixvim = {
     lsp.servers = {
-      basedpyright.enable = true;
+      basedpyright = {
+        enable = true;
+        config = {
+          settings = {
+            basedpyright = {disableOrganizeImports = true;};
+            python = {analysis = {typeCheckingMode = "recommended";};};
+          };
+        };
+      };
       ruff.enable = true;
     };
 
     plugins = {
-      lsp.servers = {
-        basedpyright = {
-          enable = true;
-          settings = {
-            basedpyright = {
-              disableOrganizeImports = true;
-            };
-            python = {
-              analysis = {
-                typeCheckingMode = "recommended";
-              };
-            };
-          };
-        };
-
-        ruff = {
-          enable = true;
-          rootMarkers = ["pyproject.toml" "ruff.toml" ".ruff.toml" ".git"];
-          onAttach.function = ''
-            client.server_capabilities.hoverProvider = false
-          '';
-        };
-      };
-
       conform-nvim.settings = {
         formatters_by_ft = {
           python = ["ruff"];
