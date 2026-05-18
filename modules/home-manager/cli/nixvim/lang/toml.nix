9a3e15ec1558e20692b83969a6657ae31756a87f
qianlitt <zvdiek@gmail.com>
2026-05-18 15:21:17 +0800

    feat(nixvim): 添加 toml 的 lsp

--- modules/home-manager/cli/nixvim/lang/toml.nix
+++ modules/home-manager/cli/nixvim/lang/toml.nix
@@ -5,7 +5,11 @@
 }: let
   inherit (config.programs.nixvim.plugins.treesitter.package) builtGrammars;
 in {
-  programs.nixvim.plugins.treesitter.grammarPackages = lib.mkAfter [
-    builtGrammars.toml
-  ];
+  programs.nixvim = {
+    lsp.servers.tombi.enable = true;
+
+    plugins.treesitter.grammarPackages = lib.mkAfter [
+      builtGrammars.toml
+    ];
+  };
 }
