97dd0138e021902b7cac6206a5778c6f4af0fd42
qianlitt <zvdiek@gmail.com>
2026-05-18 16:19:02 +0800

    feat(nixvim): 添加 xml 的 lsp

--- modules/home-manager/cli/nixvim/lang/xml.nix
+++ modules/home-manager/cli/nixvim/lang/xml.nix
@@ -5,7 +5,11 @@
 }: let
   inherit (config.programs.nixvim.plugins.treesitter.package) builtGrammars;
 in {
-  programs.nixvim.plugins.treesitter.grammarPackages = lib.mkAfter [
-    builtGrammars.xml
-  ];
+  programs.nixvim = {
+    lsp.servers.lemminx.enable = true;
+
+    plugins.treesitter.grammarPackages = lib.mkAfter [
+      builtGrammars.xml
+    ];
+  };
 }
