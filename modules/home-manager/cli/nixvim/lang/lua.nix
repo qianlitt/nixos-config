227dc79c992f6ee5ab2e5aea31cb95fbd3f5cf22
qianlitt <zvdiek@gmail.com>
2026-05-18 11:24:09 +0800

    style(nixvim): 将只有一个 lsp 的配置写成一行

--- modules/home-manager/cli/nixvim/lang/lua.nix
+++ modules/home-manager/cli/nixvim/lang/lua.nix
@@ -6,9 +6,7 @@
   inherit (config.programs.nixvim.plugins.treesitter.package) builtGrammars;
 in {
   programs.nixvim = {
-    lsp.servers = {
-      lua_ls.enable = true;
-    };
+    lsp.servers.lua_ls.enable = true;
 
     plugins.treesitter.grammarPackages = lib.mkAfter [
       builtGrammars.lua
