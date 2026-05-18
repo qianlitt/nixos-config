7e09a38e6a87f6c57bbd19e62b2af2ff56f22436
qianlitt <zvdiek@gmail.com>
2026-05-18 16:20:54 +0800

    feat(nixvim): 添加 yaml 的 lsp

--- modules/home-manager/cli/nixvim/lang/yaml.nix
+++ modules/home-manager/cli/nixvim/lang/yaml.nix
@@ -5,7 +5,11 @@
 }: let
   inherit (config.programs.nixvim.plugins.treesitter.package) builtGrammars;
 in {
-  programs.nixvim.plugins.treesitter.grammarPackages = lib.mkAfter [
-    builtGrammars.yaml
-  ];
+  programs.nixvim = {
+    lsp.servers.yamlls.enable = true;
+
+    plugins.treesitter.grammarPackages = lib.mkAfter [
+      builtGrammars.yaml
+    ];
+  };
 }
