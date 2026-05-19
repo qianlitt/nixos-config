e9fc0b165937e1ca2ffa6a68f4d57ddb7409f3da
qianlitt <zvdiek@gmail.com>
2026-05-19 17:50:25 +0800

    feat(nixvim): 添加 yaml 格式化配置

--- modules/home-manager/cli/nixvim/lang/yaml.nix
+++ modules/home-manager/cli/nixvim/lang/yaml.nix
@@ -1,12 +1,23 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }: let
   inherit (config.programs.nixvim.plugins.treesitter.package) builtGrammars;
 in {
   programs.nixvim = {
     lsp.servers.yamlls.enable = true;
+
+    plugins.conform-nvim.settings = {
+      formatters_by_ft = {
+        yaml = ["yamlfmt"];
+      };
+
+      formatters = {
+        yamlfmt.command = lib.getExe pkgs.yamlfmt;
+      };
+    };
 
     plugins.treesitter.grammarPackages = lib.mkAfter [
       builtGrammars.yaml
