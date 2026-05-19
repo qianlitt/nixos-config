21c3d694a537111fa8171f17953edfa428e3e239
qianlitt <zvdiek@gmail.com>
2026-05-19 16:59:49 +0800

    feat(nixvim): 添加 markdown 格式化配置

--- modules/home-manager/cli/nixvim/lang/markdown.nix
+++ modules/home-manager/cli/nixvim/lang/markdown.nix
@@ -1,12 +1,24 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }: let
   inherit (config.programs.nixvim.plugins.treesitter.package) builtGrammars;
 in {
   programs.nixvim = {
     lsp.servers.marksman.enable = true;
+
+    plugins.conform-nvim.settings = {
+      formatters_by_ft = {
+        markdown = ["mdformat" "prettier"];
+      };
+
+      formatters = {
+        mdformat.command = lib.getExe pkgs.mdformat;
+        prettier.command = lib.getExe' pkgs.prettier "prettier";
+      };
+    };
 
     plugins.treesitter.grammarPackages = lib.mkAfter [
       builtGrammars.markdown
