7a66983415b680c16d9a64f0a3da6ce0619c58d8
qianlitt <zvdiek@gmail.com>
2026-05-19 16:40:09 +0800

    feat(nixvim): 添加 lua 格式化配置

--- modules/home-manager/cli/nixvim/lang/lua.nix
+++ modules/home-manager/cli/nixvim/lang/lua.nix
@@ -1,12 +1,25 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }: let
   inherit (config.programs.nixvim.plugins.treesitter.package) builtGrammars;
 in {
   programs.nixvim = {
     lsp.servers.lua_ls.enable = true;
+
+    plugins.conform-nvim.settings = {
+      formatters_by_ft = {
+        lua = ["stylua"];
+      };
+
+      formatters = {
+        stylua = {
+          command = lib.getExe pkgs.stylua;
+        };
+      };
+    };
 
     plugins.treesitter.grammarPackages = lib.mkAfter [
       builtGrammars.lua
