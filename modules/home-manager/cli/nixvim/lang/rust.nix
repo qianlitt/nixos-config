21ae0804a35f98b6ac81d15945f99852372d1136
qianlitt <zvdiek@gmail.com>
2026-05-21 10:29:57 +0800

    feat(nixvim): 添加 rust 格式化配置

--- modules/home-manager/cli/nixvim/lang/rust.nix
+++ modules/home-manager/cli/nixvim/lang/rust.nix
@@ -3,6 +3,10 @@
   pkgs,
 }: {
   lsp.rust_analyzer = {};
+  conform = {
+    formatters_by_ft.rust = ["rustfmt"];
+    commands.rustfmt = lib.getExe pkgs.rustfmt;
+  };
   treesitter = ["rust"];
   extraPackages = with pkgs; [rustc cargo];
 }
