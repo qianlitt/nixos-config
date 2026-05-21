68b6b8c979f8134d93b68273f501acfd0a9e36be
qianlitt <zvdiek@gmail.com>
2026-05-21 10:44:51 +0800

    feat(nixvim): 添加 rust linter

--- modules/home-manager/cli/nixvim/lang/rust.nix
+++ modules/home-manager/cli/nixvim/lang/rust.nix
@@ -7,6 +7,7 @@
     formatters_by_ft.rust = ["rustfmt"];
     commands.rustfmt = lib.getExe pkgs.rustfmt;
   };
+  lint.rust = ["clippy"];
   treesitter = ["rust"];
-  extraPackages = with pkgs; [rustc cargo];
+  extraPackages = with pkgs; [rustc cargo clippy];
 }
