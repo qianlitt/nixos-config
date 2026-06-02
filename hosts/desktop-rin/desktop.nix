07e910d610f4666bff7101aa558d0336f8b2d745
qianlitt <zvdiek@gmail.com>
2026-06-02 08:39:19 +0800

    refactor: 将 nixos/desktop 下的模块妥善封装并规范命名

--- hosts/desktop-rin/desktop.nix
+++ hosts/desktop-rin/desktop.nix
@@ -9,9 +9,13 @@
       hyprland.enable = true;
       niri.enable = true;
     };
+
+    audio.enable = true;
+    displayManager.enable = true;
+    fcitx5.enable = true;
+    fonts.enable = true;
+    game.enable = true;
+    keyd.enable = true;
+    stylix.enable = true;
   };
-
-  modules.desktop.game.enable = true;
-
-  modules.fcitx5.enable = true;
 }
