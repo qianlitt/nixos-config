56cfa3ab2f08856b129af5d1b436f2bec94d1926
qianlitt <zvdiek@gmail.com>
2026-08-23 17:21:17 +0800

    feat(fcitx5): 替换虎码方案的数据来源
    - 将虎码 Rime 数据源从本地 `modules/overlays/fcitx5/rime-data-tiger` 切换为 `zhhmn/huma-rime` 仓库

--- modules/overlays/fcitx5/default.nix
+++ modules/overlays/fcitx5/default.nix
@@ -1,10 +1,22 @@
 {
-  flake.overlays.fcitx5 = final: prev: {
+  flake.overlays.fcitx5 = final: prev: let
+    huma-rime = prev.fetchFromGitHub {
+      owner = "zhhmn";
+      repo = "huma-rime";
+      rev = "fafb978e739fe41d91e02f35290f81c42b5f6162";
+      hash = "sha256-ArWf61NsZksOC51daXz8rW8PVI7yOKlWgQBtTEBmdpU=";
+    };
+
+    rime-data-tiger = prev.runCommand "huma-rime-rime-data" {} ''
+      mkdir -p $out/share/rime-data
+      cp -r ${huma-rime}/. $out/share/rime-data/
+    '';
+  in {
     rime-data = prev.symlinkJoin {
       name = "rime-data-custom";
       paths = [
         prev.rime-ice
-        ./rime-data-tiger
+        rime-data-tiger
       ];
     };
 
