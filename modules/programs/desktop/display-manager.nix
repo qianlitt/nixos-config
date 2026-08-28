a5dd074ebbcc64be7f796330628ec79975cf2de3
qianlitt <zvdiek@gmail.com>
2026-08-28 09:37:43 +0800

    feat: 调整 ly 日志存放路径

--- modules/programs/desktop/display-manager.nix
+++ modules/programs/desktop/display-manager.nix
@@ -11,7 +11,13 @@
     };
 
     config = lib.mkIf cfg.enable {
-      services.displayManager.ly.enable = true;
+      services.displayManager.ly = {
+        enable = true;
+        settings = {
+          ly_log = "/var/log/ly.log";
+          session_log = ".local/state/ly-session.log";
+        };
+      };
     };
   };
 }
