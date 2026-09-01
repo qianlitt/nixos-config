bb9cde9e290d9635bfb13c2a37bbd368c982e248
qianlitt <zvdiek@gmail.com>
2026-09-01 21:07:39 +0800

    feat: 添加 github-runners

--- modules/hosts/desktop-rin/services/cicd.nix
+++ modules/hosts/desktop-rin/services/cicd.nix
@@ -9,8 +9,15 @@
       quadlet.enable = true;
     };
 
+    users.groups.github-runner = {};
+    users.users.github-runner = {
+      isSystemUser = true;
+      group = "github-runner";
+    };
+
     sops.secrets = {
       "services/gitlab-runner/default" = {};
+      "services/github-runner/nixos-config" = {owner = "github-runner";};
     };
 
     # GitLab Runner
@@ -28,6 +35,15 @@
 
         dockerPullPolicy = "if-not-present";
       };
+    };
+
+    # GitHub Runner
+    services.github-runners."nixos-config" = {
+      enable = true;
+      url = "https://github.com/qianlitt/nixos-config";
+      tokenFile = config.sops.secrets."services/github-runner/nixos-config".path;
+      user = "github-runner";
+      extraLabels = ["nix"]; # workflow 用 runs-on: [self-hosted, nix]
     };
   };
 }
