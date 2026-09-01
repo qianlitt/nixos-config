c794fd2f1c27347de7983202ecc4fb128b14ace8
qianlitt <zvdiek@gmail.com>
2026-09-01 20:57:27 +0800

    refactor: 拆分 GitLab Runner 配置至 `cicd.nix`
    - 将 desktop-rin 的 GitLab Runner 配置从 `default.nix` 独立到
    `cicd.nix`。

--- modules/hosts/desktop-rin/services/default.nix
+++ modules/hosts/desktop-rin/services/default.nix
@@ -1,37 +1,7 @@
 {inputs, ...}: {
-  flake.modules.nixos.rin = {config, ...}: {
+  flake.modules.nixos.rin = {
     imports = with inputs.self.modules; [
       nixos."services.printing"
-
-      nixos."services.gitlab-runner"
     ];
-
-    modules = {
-      cli.podman = {
-        enable = true;
-        quadlet.enable = true;
-      };
-    };
-
-    sops.secrets = {
-      "services/gitlab-runner/default" = {};
-    };
-
-    # Runner 定义
-    services.gitlab-runner.services = {
-      default = {
-        authenticationTokenConfigFile = config.sops.secrets."services/gitlab-runner/default".path;
-
-        executor = "docker";
-        dockerImage = "alpine:latest";
-        description = "podman runner";
-
-        dockerVolumes = [
-          "/run/podman/podman.sock:/var/run/docker.sock"
-        ];
-
-        dockerPullPolicy = "if-not-present";
-      };
-    };
   };
 }
