bb3d6d23c2d021d17b539a0b53a6b3222af1b7e4
qianlitt <zvdiek@gmail.com>
2026-09-05 19:32:39 +0800

    feat: 调整 frieren 网络配置
    - 取消旧有的策略路由。
    - 配置 Wi-Fi 作为有线网络的备份。

--- modules/hosts/server-frieren/network.nix
+++ modules/hosts/server-frieren/network.nix
@@ -1,69 +1,86 @@
-# 这个配置文件主要 qBittorrent 下载器分流使用。
-#
-# - 主路由和 secondIp 用于下载流量（不通过旁路由进行代理）
-# - 旁路由用于其他流量
 {
-  flake.modules.nixos.frieren = let
-    mainRouter = "192.168.1.1";
-    sideRouter = "192.168.1.200";
-
-    mainIp = "192.168.1.102";
-    secondIp = "192.168.1.103";
-    prefixLength = 24;
-  in {
-    # 禁用 NetworkManager，使用 systemd-networkd
+  flake.modules.nixos.frieren = {config, ...}: {
+    # 禁用 NetworkManager
     networking = {
       networkmanager.enable = false;
       useNetworkd = true;
     };
-    systemd.network.enable = true;
 
-    # 定义自定义路由表
-    networking.iproute2 = {
+    # systemd-networkd 配置
+    systemd.network = {
       enable = true;
-      rttablesExtraConfig = ''
-        100   custom
-      '';
+      netdevs."10-bond0" = {
+        netdevConfig = {
+          Kind = "bond";
+          Name = "bond0";
+        };
+        bondConfig = {
+          Mode = "active-backup";
+          MIIMonitorSec = "1000ms";
+        };
+      };
+      networks = {
+        "30-eth0" = {
+          matchConfig.Name = "en*";
+          networkConfig.Bond = "bond0";
+        };
+        "30-wlan0" = {
+          matchConfig.Name = "wl*";
+          networkConfig.Bond = "bond0";
+          linkConfig.RequiredForOnline = "no";
+        };
+        "40-bond0" = {
+          matchConfig.Name = "bond0";
+          DHCP = "no";
+          address = [
+            "192.168.1.102/24"
+          ];
+          gateway = [
+            "192.168.1.200"
+          ];
+          dns = [
+            "192.168.1.200"
+          ];
+          linkConfig.RequiredForOnline = "carrier";
+        };
+      };
     };
 
-    # 配置网络接口、地址、路由和策略路由规则
-    systemd.network.networks."10-eth0" = {
-      matchConfig.Name = "en*";
-
-      networkConfig = {
-        Description = "Wired Ethernet Connection";
-        DHCP = "no";
-        LinkLocalAddressing = "no";
+    # Wi-Fi 配置
+    # 禁用 wpa_supplicant，使用 iwd
+    networking.wireless = {
+      enable = false;
+      iwd = {
+        enable = true;
+        settings = {
+          General = {
+            EnableNetworkConfiguration = false;
+          };
+        };
       };
+    };
 
-      address = [
-        "${mainIp}/${toString prefixLength}"
-        "${secondIp}/${toString prefixLength}"
-      ];
+    sops.secrets."wifi/home/psk" = {};
+    sops.templates."GONGNIU-7490.psk" = {
+      content = ''
+        [Security]
+        Passphrase=${config.sops.placeholder."wifi/home/psk"}
 
-      dns = [
-        sideRouter
-        mainRouter
+        [Settings]
+        AutoConnect=true
+      '';
+    };
+    systemd = {
+      tmpfiles.rules = [
+        "d /var/lib/iwd 0700 root root -"
       ];
-
-      # 路由：
-      # - 第一条不指定 Table，默认加入 main 表（旁路由网关）
-      # - 第二条指定 Table = "custom"，仅用于 custom 表
-      routes = [
-        {Gateway = sideRouter;} # main 表默认路由
-        {
-          Gateway = mainRouter;
-          Table = "100";
-        }
-      ];
-
-      # 策略路由规则：让 secondIp 的流量走 custom 表
-      routingPolicyRules = [
-        {
-          From = "${secondIp}";
-          Table = "100";
-        }
-      ];
+      # 在 iwd 启动前创建链接
+      services.iwd = {
+        preStart = ''
+          ln -sf ${config.sops.templates."GONGNIU-7490.psk".path} /var/lib/iwd/GONGNIU-7490.psk
+        '';
+        restartTriggers = [config.sops.secrets."wifi/home/psk".path];
+      };
     };
   };
 }
