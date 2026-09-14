d3e2365e070afdffc2722522f575b9dfc3336fea
qianlitt <zvdiek@gmail.com>
2026-09-14 19:05:58 +0800

    feat(nix): 配置 frieren 定时重启以更新系统

--- modules/hosts/server-frieren/config.nix
+++ modules/hosts/server-frieren/config.nix
@@ -15,5 +15,28 @@
 
       i18n.enable = true;
     };
+
+    # 每周一更新 flake.lock，然后通过 `nixos-rebuild boot` 部署到该主机
+    # 每周二自动重启以更新系统
+    systemd.services.auto-reboot = {
+      description = "每周二自动重启";
+      script = ''
+        systemctl reboot
+      '';
+      serviceConfig = {
+        Type = "oneshot";
+        User = "root";
+      };
+    };
+
+    systemd.timers.auto-reboot = {
+      wantedBy = ["timers.target"];
+      timerConfig = {
+        # 每周二 00:00 触发
+        OnCalendar = "Tue 00:00:00";
+        Persistent = false;
+        RandomizedDelaySec = "5m";
+      };
+    };
   };
 }
